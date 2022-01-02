2d57fda74b69037452c6810ac50ce7aa7048794e
quanbaphuongnam <nam.qbp29@aptecchlearning.edu.vn>
2022-01-02 10:47:01 +0700

    nam cap nhat layout

---
+++ AuctionHunter/src/main/java/com/demo/controllers/ContactUsController.java
@@ -0,0 +1,15 @@
+package com.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="contactus")
+public class ContactUsController {
+	@RequestMapping(value={"","index"}, method = RequestMethod.GET)
+	public String index() {	
+	
+		return "user/contactus/index";
+	}
+}
---
+++ AuctionHunter/src/main/java/com/demo/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.demo.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value={"","home"})
+public class HomeController {
+	@RequestMapping(value={"","index"}, method = RequestMethod.GET)
+	public String index() {	
+	
+		return "user/home/index";
+	}
+}
